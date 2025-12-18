f3a65358f3396de4899767e7a0b5ba701111e1e6
SAKTHI GANESH S AI-DS <sakthiganesh.s2024aids@sece.ac.in>
2025-12-18 21:54:54 +0530

    Update login API endpoint to production URL

--- Missing-Person-Tracker/frontend/src/components/Login.js
+++ Missing-Person-Tracker/frontend/src/components/Login.js
@@ -6,7 +6,7 @@ export default function Login({ onLogin, onSwitch }) {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post('http://localhost:5000/api/login', form);
+      const res = await axios.post('https://missing-person-finder-xobu.onrender.com/', form);
       onLogin(res.data);
     } catch {
       alert("Invalid email or password.");
@@ -44,4 +44,5 @@ export default function Login({ onLogin, onSwitch }) {
       </div>
     </div>
   );
+
 }
